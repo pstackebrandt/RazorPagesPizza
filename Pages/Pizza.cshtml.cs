268b19ebbee14450d24571d4d6fff681a8e780f7
Peter Stackebrandt <peter.stackebrandt@googlemail.com>
2022-01-14 17:22:14 +0100

    Add Property NewPizza to PizzaModel - contains further 3 errors

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
 
 namespace RazorPagesPizza.Pages
 {
     public class PizzaModel : PageModel
     {
+        [BindProperty]
+        public Pizza NewPizza {get; set;}
+
         public void OnGet()
         {
+            
         }
     }
 }
